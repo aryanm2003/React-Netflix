563a99eba2d2e098d42c7c8ade19f7064fc79d81
aryanm2003 <128585610+aryanm2003@users.noreply.github.com>
2024-06-11 13:14:28 +0530

    initial

---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
+const Player = () => {
+  const {id} = useParams();
+  const navigate = useNavigate();
+
+  const [apiData,setApiData] = useState({
+    name:"",
+    key:"",
+    published_at:"",
+    typeof:""
+  });
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNDY1ZGRlMmI4YmRjZGNjMWZhMzI2NjgzZDE2MTgxZCIsInN1YiI6IjY2NjdkN2QxMGM1NDgxZWM5NGY3NmI1NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.toKM7QF_vlPDquvkm65FR2ivUsT8o0dBSEEs3l6dRas'
+    }
+  };
+  
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results[0]))
+    .catch(err => console.error(err));
+  },[])
+
+  return (
+    <div className='player'>
+      <img src={back_arrow_icon} alt=""  onClick={()=>{navigate(-2)}}/>
+      <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`} title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+
+      </div>
+    </div>
+  )
+}
+
+export default Player
